4953b57b640612e019a544c835ef02bf1f5c84c2
eranee <eranee@users.noreply.github.com>
2019-12-12 20:10:17 +0100

    Added Biogeographic preliminary article

--- Beetle Scripts/DiscoveryCurvePlot.R
+++ Beetle Scripts/DiscoveryCurvePlot.R
@@ -1,120 +1,70 @@
-par(mfrow=c(3,3),mar=c(2,2,2,2))
+library(tidyverse)
+library(stringr)
 
-Chios_f <- Chios_curve[["f.stats"]]
-plot(Chios_curve[["rarefact.line"]],xlim=c(50,5000),ylim=c(1,250),
-     type="l",lwd=2,col="#B66F34",xlab="",ylab="", main="Chios")
-#points(Chios_curve[["rarefact.line"]],type="l",lwd=2,col="#B66F34")
-points(Chios_curve[["extrap.line"]],type="l",lty=2,lwd=2,col="#B66F34")
-points(Chios_f[[4]],Chios_f[[3]],type="p",pch=23, bg="#B66F34", col="#B66F34")
-points(Chios_curve[["ci.poly"]],type="l",lty=6,lwd=2, col="#B66F34")
-
-Crete_f <- Crete_curve[["f.stats"]]
-plot(Crete_curve[["rarefact.line"]],xlim=c(50,5000),ylim=c(1,250),
-     type="l",lwd=2,col="#afeeee",xlab="",ylab="",main="Crete")
-#points(Crete_curve[["rarefact.line"]],type="l",lwd=2,col="#afeeee")
-points(Crete_curve[["extrap.line"]],type="l",lty=2,lwd=2,col="#afeeee")
-points(Crete_f[[4]],Crete_f[[3]],type="p",pch=23, bg="#afeeee", col="#afeeee")
-points(Crete_curve[["ci.poly"]],type="l",lty=6,lwd=2, col="#afeeee")
-
-#points(Corfu_curve[["rarefact.line"]],type="l",lwd=2,col="#C9F52A")
-#points(Corfu_curve[["extrap.line"]],type="l",lty=2,lwd=2,col="#C9F52A")
-
-Ikaria_f <- Ikaria_curve[["f.stats"]]
-plot(Ikaria_curve[["rarefact.line"]],xlim=c(50,5000),ylim=c(1,250),
-     type="l",lwd=2,col="#ffd1dc",xlab="",ylab="",main="Ikaria")
-#points(Ikaria_curve[["rarefact.line"]],type="l",lwd=2,col="#ffd1dc")
-points(Ikaria_curve[["extrap.line"]],type="l",lwd=2,lty=2,col="#ffd1dc")
-points(Ikaria_f[[4]],Ikaria_f[[3]],type="p",pch=23, bg="#ffd1dc", col="#ffd1dc")
-points(Ikaria_curve[["ci.poly"]],type="l",lty=6,lwd=2, col="#ffd1dc")
-
-Karpathos_f <- Karpathos_curve[["f.stats"]]
-plot(Karpathos_curve[["rarefact.line"]],xlim=c(50,5000),ylim=c(1,250),
-     type="l",lwd=2,col="#ffa500",xlab="",ylab="",main="Karpathos")
-#points(Karpathos_curve[["rarefact.line"]],type="l",lwd=2,col="#ffa500")
-points(Karpathos_curve[["extrap.line"]],type="l",lwd=2,lty=2,col="#ffa500")
-points(Karpathos_f[[4]],Karpathos_f[[3]],type="p",pch=23, bg="#ffa500", col="#ffa500")
-points(Karpathos_curve[["ci.poly"]],type="l",lty=6,lwd=2, col="#ffa500")
-
-Kos_f <- Kos_curve[["f.stats"]]
-plot(Kos_curve[["rarefact.line"]],xlim=c(50,5000),ylim=c(1,250),
-     type="l",lwd=2,col="#A61C86",xlab="",ylab="",main="Kos")
-#points(Kos_curve[["rarefact.line"]],type="l",lwd=2,col="#A61C86")
-points(Kos_curve[["extrap.line"]],type="l",lwd=2,lty=2,col="#A61C86")
-points(Kos_f[[4]],Kos_f[[3]],type="p",pch=23, bg="#A61C86", col="#A61C86")
-points(Kos_curve[["ci.poly"]],type="l",lty=6,lwd=2, col="#A61C86")
-
-Lesbos_f <- Lesbos_curve[["f.stats"]]
-plot(Lesbos_curve[["rarefact.line"]],xlim=c(50,5000),ylim=c(1,250),
-     type="l",lwd=2,col="#591AF3",xlab="",ylab="",main="Lesbos")
-#points(Lesbos_curve[["rarefact.line"]],type="l",lwd=2,col="#591AF3")
-points(Lesbos_curve[["extrap.line"]],type="l",lwd=2,lty=2,col="#591AF3")
-points(Lesbos_f[[4]],Lesbos_f[[3]],type="p",pch=23, bg="#591AF3", col="#591AF3")
-points(Lesbos_curve[["ci.poly"]],type="l",lty=6,lwd=2, col="#591AF3")
-
-Rhodes_f <- Rhodes_curve[["f.stats"]]
-plot(Rhodes_curve[["rarefact.line"]],xlim=c(50,5000),ylim=c(1,250),
-     type="l",lwd=2,col="black",xlab="",ylab="",main="Rhodes")
-#points(Rhodes_curve[["rarefact.line"]],type="l",lwd=2,col="black")
-points(Rhodes_curve[["extrap.line"]],type="l",lwd=2,lty=2,col="black")
-points(Rhodes_f[[4]],Rhodes_f[[3]],type="p",pch=23, bg="black", col="black")
-points(Rhodes_curve[["ci.poly"]],type="l",lty=6,lwd=2, col="black")
-
-Samos_f <- Samos_curve[["f.stats"]]
-plot(Samos_curve[["rarefact.line"]],xlim=c(50,5000),ylim=c(1,250),
-     type="l",lwd=2,col="#d3d3d3",xlab="",ylab="",main="Samos")
-#points(Samos_curve[["rarefact.line"]],type="l",lwd=2,col="#d3d3d3")
-points(Samos_curve[["extrap.line"]],type="l",lwd=2,lty=2,col="#d3d3d3")
-points(Samos_f[[4]],Samos_f[[3]],type="p",pch=23, bg="#d3d3d3", col="#d3d3d3")
-points(Samos_curve[["ci.poly"]],type="l",lty=6,lwd=2, col="#d3d3d3")
-
-Skyros_f <- Skyros_curve[["f.stats"]]
-plot(Skyros_curve[["rarefact.line"]],xlim=c(50,5000),ylim=c(1,250),
-     type="l",lwd=2,col="#00ff00",xlab="",ylab="",main="Skyros")
-#points(Skyros_curve[["rarefact.line"]],type="l",lwd=2,col="#00ff00")
-points(Skyros_curve[["extrap.line"]],type="l",lwd=2,lty=2,col="#00ff00")
-points(Skyros_f[[4]],Skyros_f[[3]],type="p",pch=23, bg="#00ff00", col="#00ff00")
-points(Skyros_curve[["ci.poly"]],type="l",lty=6,lwd=2, col="#00ff00")
-
-##COMPARATIVE PLOT##
-par(mfrow=c(1,1))
-plot(Chios_curve[["rarefact.line"]],xlim=c(50,5000),ylim=c(1,250),
-     type="l",lwd=2,col="#B66F34",xlab="Samples",ylab="Species")
-points(Chios_curve[["extrap.line"]],type="l",lty=2,lwd=2,col="#B66F34")
-points(Chios_f[[4]],Chios_f[[3]],type="p",pch=23, bg="#B66F34", col="#B66F34")
-
-points(Crete_curve[["rarefact.line"]],type="l",lwd=2,col="#afeeee")
-points(Crete_curve[["extrap.line"]],type="l",lty=2,lwd=2,col="#afeeee")
-points(Crete_f[[4]],Crete_f[[3]],type="p",pch=23, bg="#afeeee", col="#afeeee")
-
-points(Ikaria_curve[["rarefact.line"]],type="l",lwd=2,col="#ffd1dc")
-points(Ikaria_curve[["extrap.line"]],type="l",lwd=2,lty=2,col="#ffd1dc")
-points(Ikaria_f[[4]],Ikaria_f[[3]],type="p",pch=23, bg="#ffd1dc", col="#ffd1dc")
-
-points(Karpathos_curve[["rarefact.line"]],type="l",lwd=2,col="#ffa500")
-points(Karpathos_curve[["extrap.line"]],type="l",lwd=2,lty=2,col="#ffa500")
-points(Karpathos_f[[4]],Karpathos_f[[3]],type="p",pch=23, bg="#ffa500", col="#ffa500")
-
-points(Kos_curve[["rarefact.line"]],type="l",lwd=2,col="#A61C86")
-points(Kos_curve[["extrap.line"]],type="l",lwd=2,lty=2,col="#A61C86")
-points(Kos_f[[4]],Kos_f[[3]],type="p",pch=23, bg="#A61C86", col="#A61C86")
-
-points(Lesbos_curve[["rarefact.line"]],type="l",lwd=2,col="#591AF3")
-points(Lesbos_curve[["extrap.line"]],type="l",lwd=2,lty=2,col="#591AF3")
-points(Lesbos_f[[4]],Lesbos_f[[3]],type="p",pch=23, bg="#591AF3", col="#591AF3")
-
-points(Rhodes_curve[["rarefact.line"]],type="l",lwd=2,col="black")
-points(Rhodes_curve[["extrap.line"]],type="l",lwd=2,lty=2,col="black")
-points(Rhodes_f[[4]],Rhodes_f[[3]],type="p",pch=23, bg="black", col="black")
-
-points(Samos_curve[["rarefact.line"]],type="l",lwd=2,col="#d3d3d3")
-points(Samos_curve[["extrap.line"]],type="l",lwd=2,lty=2,col="#d3d3d3")
-points(Samos_f[[4]],Samos_f[[3]],type="p",pch=23, bg="#d3d3d3", col="#d3d3d3")
-
-points(Skyros_curve[["rarefact.line"]],type="l",lwd=2,col="#00ff00")
-points(Skyros_curve[["extrap.line"]],type="l",lwd=2,lty=2,col="#00ff00")
-points(Skyros_f[[4]],Skyros_f[[3]],type="p",pch=23, bg="#00ff00", col="#00ff00")
-
-legend(50, 265, c("Crete","Chios","Ikaria","Kos","Lesbos","Rhodes","Samos","Skyros","Karpathos"),
-       cex=0.7, col=c("#afeeee","#B66F34","#ffd1dc","#A61C86","#591AF3","black","#d3d3d3","#00ff00","#ffa500"),pch=23,lty=1)
-title(main="Species Discovery curves for various Aegean islands",xlab="Samples",ylab="Species")
-
+### Preparing R for the script, importing data ###
+oldw <- getOption("warn")
+options(warn = -1)
+rm(temp,RLine, ELine,STDLine,x,y,Location,n2,z,a, i, MaxPoints)
+temp = as.character(objects(pattern="_curve"))
+RLine <- data.frame()
+MaxPoints <- data.frame()
+ELine <- data.frame()
+STDLine <- data.frame()
+### Preparing data for plotting ###
+for (i in seq_along(temp)){
+  ### Defining data ###
+  x <- get(temp[i])
+  y <- deparse(temp[i])
+  Location <- str_remove(y,"_curve")
+  Location <- str_remove_all(Location,"\"")
+  zr <- as.data.frame(x[["rarefact.line"]])
+  ze <- as.data.frame(x[["extrap.line"]])
+  zstd <- as.data.frame(x[["ci.poly"]])
+  names(zr) <- c("Samples", "Species")
+  names(ze) <- c("Samples", "Species")
+  names(zstd) <- c("Samples", "Species")
+  m1 <- data.frame(as.numeric(as.character(max(zr$Samples))),as.numeric(as.character(max(zr$Species))), Location)
+  names(m1) <- c("Samples", "Species", "Location")
+  ### Shaping the data ###
+  ar <- cbind(zr, Location)
+  ae <- cbind(ze, Location)
+  astd <- cbind(zstd, Location)
+  RLine <- bind_rows(RLine,ar)
+  ELine <- bind_rows(ELine,ae)
+  STDLine <- bind_rows(STDLine, astd)
+  MaxPoints <- bind_rows(m1,MaxPoints)
+}
+options(warn = oldw)
+### Comparative ###
+comp <- ggplot(data=RLine, aes(x=Samples, y=Species, colour=Location,alpha=0.6)) +
+  geom_line(size=1.2)+ 
+  geom_line(data=ELine, aes(x=Samples, y=Species, colour=Location), linetype="twodash",size=1.2) +
+  geom_point(data=MaxPoints, aes(x=Samples, y=Species, colour = Location), alpha =1, size=2) +
+  geom_segment(data=MaxPoints, aes(x=Samples, y=Species, xend=Samples, yend=0), size=1.05) + 
+  geom_segment(data=MaxPoints, aes(x=Samples, y=Species, xend=0, yend=Species), size=1.05) + 
+  scale_x_continuous(expand = c(0, 0),breaks=seq(0,7000,500)) + 
+  scale_y_continuous(expand = c(0, 0),breaks=seq(0,275,25)) +
+  coord_cartesian(xlim =c(0, 7000), ylim = c(0, 275)) +
+  #scale_alpha(show.legend=FALSE)+
+  guides(alpha = FALSE) +
+  scale_color_discrete(name="", guide = guide_legend(override.aes=list(shape=15,size=7))) +
+  ggtitle("Comparative Species Richness Estimates") +
+  theme(legend.margin = margin(-0.95,0,0,0, unit="cm"),plot.title=element_text(hjust=0.5,face="bold"))
+comp
+ggsave("comp_500dpi.png", comp, height = 10, width = 15, dpi = 500)
+### Individual Plot ###
+ind <- ggplot(data=RLine, aes(x=Samples,y=Species,colour=Location)) +
+  geom_polygon(data=STDLine, aes(x=Samples, y=Species, colour=Location),alpha=0.1) +
+  geom_line(size=1.1)+
+  geom_line(data=ELine, aes(x=Samples, y=Species, colour=Location), linetype="dotdash",size=1.1) +
+  geom_point(data=MaxPoints, aes(x=Samples, y=Species),colour="black", size=1.5) +
+  geom_segment(data=MaxPoints, aes(x=Samples, y=Species, xend=Samples, yend=0), color="black", size=1.05) + 
+  geom_segment(data=MaxPoints, aes(x=Samples, y=Species, xend=0, yend=Species), color="black", size=1.05) + 
+  scale_x_continuous(expand = c(0, 0),breaks=seq(0,7000,1000)) + 
+  scale_y_continuous(expand = c(0, 0),breaks=seq(0,275,25)) +
+  coord_cartesian(xlim =c(0, 7000), ylim = c(0, 275)) +
+  facet_wrap(Location ~ ., ncol=2)+ 
+  ggtitle("Species Richness Estimates") + 
+  theme(legend.position = "none", plot.title =element_text(hjust=0.5,face="bold"),panel.spacing.x=unit(6,"mm"))
+ind
+ggsave("ind_1000dpi.png", ind, height = 15, width = 10, dpi = 1000)
